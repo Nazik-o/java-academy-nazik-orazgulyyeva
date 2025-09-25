393acd879563e8b8aa116ada29dcb81ae5cced04
Nazik-o <meimeilovessansan@gmail.com>
2025-09-25 10:52:10 -0700

    Fully finished with Math excercises

--- workbook-1d/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ workbook-1d/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -1,16 +1,14 @@
+package com.pluralsight;
+
 public class MathApp {
     public static void main(String[] args) {
 // Question 1:
 // declare variables here
         //Question: 1
-        String person1 = "bobSalary";
-        String person2 = "garySalary";
         double bobSalary = 80000;
         double garySalary = 100000;
-        String highestSalary = Math.max(bobSalary,garySalary);
-        String lowestSalary = Math.min(bobSalary,garySalary);
+        double highestSalary = Math.max(bobSalary,garySalary);
         System.out.println("The highest salary is " + highestSalary);
-        System.out.println("The lowest salary is " + lowestSalary);
 
 
         //Question 2:
@@ -18,37 +16,53 @@ public class MathApp {
         int truckPrice = 200000;
         System.out.println("The car price is :" + carPrice);
         System.out.println("The truck price is :" + truckPrice);
+        int smallestPrice = Math.min(carPrice, truckPrice);
+        System.out.println("The smallest price is: " + smallestPrice);
 
         //Question 3:
-        String areaOfTheCircle = "A = pi * r^2";
-        char area = 'A';
+        double radius = 7.25;
+        double areaOfTheCircle = Math.PI * Math.pow(radius, 2);
+        //"A = pi * r^2"
+        System.out.println("The area of a circle with radius 7.25 is: " + areaOfTheCircle);
+        /*char area = 'A';
         double pie = 3.1415;
-        double givenRadius = 7.25;
         double radius = givenRadius * 2;
         String findSolution;
         findSolution = (area == pie * (givenRadius + radius);
-        System.out.println("Solution to find Area of the circle is :" + findSolution);
+        System.out.println("Solution to find Area of the circle is :" + findSolution);*/
 
         //Question 4:
         double givenNumber = 5.0;
-        double squareRoot = givenNumber * 2;
-        System.out.println("The square root of 5.0 is = " + squareRoot);
+        double squareRoot = Math.sqrt(givenNumber);
+        System.out.println("The square root of " + givenNumber + "is = " + squareRoot);
 
         //Question 5:
-        double distanceFormula = (sqrt((x2 - x1)^2)) + (sqrt((y2 - y1)^2));
         double x1 = 5;
         double y1 = 10;
         double x2 = 85;
         double y2 = 50;
-        double findDistance = distanceFormula;
-        System.out.println("Distance between points " + findDistance);
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("Distance between points " + distance);
 
         //Question 6:
-        double number = -3.8;
-        double;
+        double negativeValue = -3.8;
+        double absValue = Math.abs(negativeValue);
+        System.out.println("The absolute value of " + negativeValue + " is: " + absValue);
+
         //Question 7:
-        double randomNumbers = (1,2,3,4,5) =  (int) (Math.random() * 10);
-        System.out.println("Random number :" + randomNumbers );
+        double randomNumbers = Math.random();
+        System.out.println("Random number between 0 and 1 is :" + randomNumbers );
+
+        //Question 8:
+        int minutesInHour = 60;
+        int hoursInDay = 24;
+        int days = 24;
+
+        int minutesIn24Days = minutesInHour * hoursInDay * days;
+        long millisecondsIn24Days = minutesIn24Days * 60L * 1000L;
+
+        System.out.println("Minutes in 24 days: " + minutesIn24Days);
+        System.out.println("Milliseconds in 24 days: " + millisecondsIn24Days);
 
 
 
