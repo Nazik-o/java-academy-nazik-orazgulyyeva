f4b1dfbefa17c43b715642a30f572da8f531e36c
Nazik-o <meimeilovessansan@gmail.com>
2025-09-25 10:02:27 -0700

    Created the movie night for my friends

---
+++ workbook-1d/StringApplication/src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class VariableApp {
+    public static void main(String[] args) {
+
+// declare variables here
+                //STEP 1:
+                String myFavoriteColor = "Purple";
+                int year = 2025;
+                char middleInitial = 'D';
+                boolean pet = true;
+                System.out.println("My favorite color is :" + myFavoriteColor + "." );
+                System.out.println("Current year is :" + year);
+                System.out.println("I have a dog" + pet);
+                System.out.println("My middle initial starts with " + middleInitial );
+                //STEP 2:
+                int daysInWeek = 7;
+                double priceOfCoffee = 4.99;
+                char myFavoriteLetter = 'A';
+                boolean isRaining = true;
+                System.out.println("There are:" + daysInWeek + "in a week");
+                System.out.println("Price per coffe is:" + priceOfCoffee + "$");
+                System.out.println("My favorite letter is:" + myFavoriteLetter);
+                System.out.println("Is it raining today? - " + isRaining);
+
+            }
+}
