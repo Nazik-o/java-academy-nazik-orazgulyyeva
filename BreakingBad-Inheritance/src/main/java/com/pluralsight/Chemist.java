161290848c52389c861de291f099219a5418df8b
Nazik-o <meimeilovessansan@gmail.com>
2025-10-28 14:33:25 -0700

    Breaking Bad practice

---
+++ BreakingBad-Inheritance/src/main/java/com/pluralsight/Chemist.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Chemist {
+    public void cook(){
+        System.out.println("Cooking something ordinary.");
+    }
+
+
+
+}
---
+++ BreakingBad-Inheritance/src/main/java/com/pluralsight/JessePinkman.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class JessePinkman extends Chemist{
+    @Override
+    public void cook() {
+        System.out.println("Yeah science!");
+    }
+}
